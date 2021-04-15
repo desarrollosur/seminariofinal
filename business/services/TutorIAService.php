74d7e06274f5f15c2d949960b1cc9537aa663a60
desarrollosur <desarrollosur@gmail.com>
2021-04-15 10:13:02 -0300

    TG-40 mejora en respuesta desde tutor

--- business/services/TutorIAService.php
+++ business/services/TutorIAService.php
@@ -7,6 +7,7 @@ use app\models\Cuestionario;
 use app\models\User;
 use GuzzleHttp\Client;
 use Yii;
+use yii\helpers\Json;
 
 /**
  * @property Client $cliente cliente guzzle
@@ -22,9 +23,33 @@ class TutorIAService implements IServicioTutorIA{
     }
     //put your code here
     public function consultarTutorUsuarioCuestionario(User $usuarioActual, Cuestionario $cuestionario) {
-        $consultaTutor = (string) $this->cliente->get('http://flask:5000')->getBody();
+        
+        $paramEjemplo = <<<EOL
+                {
+                "order_id": [1, 2, 3, 4, 5, 6, 7, 8],
+                "user_id": [1, 1, 1, 1, 1, 1, 1, 1],
+                "skill_name": [
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions",
+                    "Calculate part in proportion with fractions"
+                    ],
+                "correct": [1,1,1,0,1,1,1,1]
+                }
+        EOL;
+        
+        $parametros = Json::decode($paramEjemplo);
+        $consultaTutor = (string)$this->cliente->post('http://flask:5000/predict', ['json'=>$parametros])->getBody();
         Yii::error($consultaTutor);
         return $consultaTutor;
+    }
+    
+    private function extraerParametrosDeCuestionario(User $usuarioActual, Cuestionario $cuestionario){
+        
     }
 
 }
